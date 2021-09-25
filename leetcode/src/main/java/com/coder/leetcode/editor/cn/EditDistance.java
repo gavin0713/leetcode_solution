b1d17f2a87476c86345de28160f6e6bede9a5c3d
Gavin <while@whu.edu.cn>
2021-09-25 21:33:34 +0800

    add leetcode module

---
+++ leetcode/src/main/java/com/coder/leetcode/editor/cn/EditDistance.java
@@ -0,0 +1,59 @@
+//Given two strings word1 and word2, return the minimum number of operations 
+//required to convert word1 to word2. 
+//
+// You have the following three operations permitted on a word: 
+//
+// 
+// Insert a character 
+// Delete a character 
+// Replace a character 
+// 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: word1 = "horse", word2 = "ros"
+//Output: 3
+//Explanation: 
+//horse -> rorse (replace 'h' with 'r')
+//rorse -> rose (remove 'r')
+//rose -> ros (remove 'e')
+// 
+//
+// Example 2: 
+//
+// 
+//Input: word1 = "intention", word2 = "execution"
+//Output: 5
+//Explanation: 
+//intention -> inention (remove 't')
+//inention -> enention (replace 'i' with 'e')
+//enention -> exention (replace 'n' with 'x')
+//exention -> exection (replace 'n' with 'c')
+//exection -> execution (insert 'u')
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 0 <= word1.length, word2.length <= 500 
+// word1 and word2 consist of lowercase English letters. 
+// 
+// Related Topics 字符串 动态规划 👍 1834 👎 0
+
+package com.coder.leetcode.editor.cn;
+public class EditDistance {
+    public static void main(String[] args) {
+        Solution solution = new EditDistance().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int minDistance(String word1, String word2) {
+        return 0;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
